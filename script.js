3f2cc03bcf95001a6572600912b0b09cb995aa9e
misschootemup <misschootemup@gmail.com>
2023-10-23 18:53:10 -0500

    Added some java script

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 var timerInterval;
 var secondsLeft = 10;
 var timeEl = document.querySelector("#Clock");
+var question = [
+  {
+    question: "what question goes here?",
+    options: [" A", "B", "C", "D"],
+    key: "B"
+  },
+  {
+    question: "what question goes here?",
+    options: [" A", "B", "C", "D"],
+    key: "B"
+  },
+  {
+    question: "what question goes here?",
+    options: [" A", "B", "C", "D"],
+    key: "B"
+  },
+  {
+    question: "what question goes here?",
+    options: [" A", "B", "C", "D"],
+    key: "B"
+  },
+
+]
+var level = 0
+var questionElements =document.getElementById("Question")
+var answerElements =document.getElementById("Answer-Generator")
 
 var StartButton = document.getElementById(
     "Start-button"
@@ -10,8 +36,11 @@ StartButton.addEventListener(
         document.getElementById("Welcome").classList.add("Hidden")
         document.getElementById("Quiz").classList.remove("Hidden")
         startTimer()
+        QuestionDisplay()
     }
 )
+
+
 function startTimer(){
      timerInterval = setInterval(function() {
         secondsLeft--;
@@ -24,4 +53,24 @@ function startTimer(){
         }
     
       }, 1000);
+}
+function QuestionDisplay(){
+  questionElements.textContent=question[level].question
+  answerElements.innerHTML=""
+  for (var i = 0; i < question [level].options.length; i++) {
+    var button=document.createElement("button")
+    button.textContent=question [level].options[i]
+    button.onclick = AnswerCheck
+answerElements.append(button)
+  }
+}
+function AnswerCheck(){
+  console.log(this)
+  if (this.textContent===question [level].key) {
+    console.log ("correct")
+    } else {
+    console.log("wrong")
+  }
+  level++
+  QuestionDisplay()
 }
